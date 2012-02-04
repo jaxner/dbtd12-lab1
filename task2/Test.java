4f9d7e5aa035eede36ddec67c85cbc468fb5b5c4
Joakim Axnér <jaxner@kth.se>
2012-02-04 01:21:18 +0100

    Add code skeleton for an overriding select() function

--- task2/Test.java
+++ task2/Test.java
@@ -18,4 +18,14 @@ public class Test extends Interface{
     public void remove(){
 
     }
+    
+    @Override
+    public void select() {
+        /*
+         * TODO:
+         * Modify the select version in Interface.java so that it uses
+         * transactions
+         */
+        super.select();
+    }
 }
