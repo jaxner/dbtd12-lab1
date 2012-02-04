0bd689ddf7a96fedd778d355cf20ef96e44899a0
Joakim Axnér <jaxner@kth.se>
2012-02-04 01:35:56 +0100

    Add initial code for insert() function

--- task2/Test.java
+++ task2/Test.java
@@ -6,7 +6,20 @@ public class Test extends Interface{
 
     @Override
     public void insert(){
+        String table_name = getStringPrompt("Which table?");
+        String columns = getStringPrompt("Which columns?");
+        String values = getStringPrompt("Which values?");
+        String query = "INSERT INTO "+ table_name
+            + " (" + columns + ") VALUES (" + values + ");";
 
+        System.out.println("Executing query: " + query);
+
+        try {
+            getStatement().executeUpdate(query);
+        } catch (SQLException e) {
+            System.out.println("Invalid syntax!");
+            System.out.println(e);
+        }
     }
 
     @Override
